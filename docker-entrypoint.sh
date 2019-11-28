69ee930f466da7d2a5d3217502225ccd6c0d9d74
Concision <contact@concision.me>
2019-11-28 11:45:15 -0800

    Added tighter Windscribe username and password sanity checks Fixed process substitutions causing no output to occur if an error occurred

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,85 +3,97 @@
 set -e
 
 ### Formatting
-# time formatting
-exec 2> >(while read -r line; do echo "$(date '+%Y-%m-%d %H:%M:%S') ERR $line"; done) \
-      > >(while read -r line; do echo "$(date '+%Y-%m-%d %H:%M:%S') OUT $line"; done)
-
 # command prefixing (source: https://unix.stackexchange.com/a/440514)
 prefixWith() {
-    local prefix="$1"
-    shift
+	local prefix="$1"
+	shift
 
-    # redirect standard input and error to different commands (source: https://stackoverflow.com/a/31151808)
-    { "$@" 2>&1 1>&3 3>&- | { while read -r line; do  echo "$prefix $line"; done; }; } 3>&1 1>&2 | { while read -r line; do  echo "$prefix $line"; done; }
+	# redirect standard input and error to different commands (source: https://stackoverflow.com/a/31151808)
+	{ "$@" 2>&1 1>&3 3>&- | { while read -r line; do  echo "$prefix $line"; done; }; } 3>&1 1>&2 | { while read -r line; do  echo "$prefix $line"; done; }
 }
 
-
-### Sanity Checks
-if [[ -z "${WINDSCRIBE_USERNAME}" ]]; then
-  	prefixWith "[WINDSCRIBE]" echo "Unset Windscribe username; ensure that the environment variable \$WINDSCRIBE_USERNAME is set "
-	  exit 1
-fi
-if [[ -z "${WINDSCRIBE_PASSWORD}" ]]; then
-  	prefixWith "[WINDSCRIBE]" echo "Unset Windscribe password; ensure that the environment variable \$WINDSCRIBE_PASSWORD is set "
-	  exit 1
-fi
-iptables -vnL > /dev/null 2>&1 || {
-    prefixWith "[IPTABLES]" echo "Ensure cap_add is set for both NET_RAW and NET_ADMIN"
-	  exit 1
-}
-
-
-### Create SSH identity and start OpenSSH server
-# manage SSH identity
-prefixWith "[SSH]" echo "Creating SSH key"
-# generate SSH key
-prefixWith "[SSH]" ssh-keygen -N "" -f "/root/.ssh/id_rsa" <<< y
-# allow self-connecting with SSH
-prefixWith "[SSH]" cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
-# start SSH server
-prefixWith "[SSH]" echo "Starting OpenSSH server"
-prefixWith "[SSH]" service ssh start
-
-
-### Create TUN device for Windscribe
-# create TUN device
-prefixWith "[TUN]" echo "Creating TUN device"
-prefixWith "[TUN]" mkdir -p /dev/net
-prefixWith "[TUN]" mknod /dev/net/tun c 10 200
-prefixWith "[TUN]" chmod 600 /dev/net/tun
+{ {
+	### Sanity Checks
+	# username checks
+	if [[ -z "${WINDSCRIBE_USERNAME}" ]]; then
+	  	prefixWith "[WINDSCRIBE]" echo "Unset Windscribe username; ensure that the environment variable \$WINDSCRIBE_USERNAME is set properly"
+		  exit 1
+	fi
+	if [[ "${WINDSCRIBE_USERNAME}" =~ [^a-zA-Z0-9_] ]]; then
+	  	prefixWith "[WINDSCRIBE]" echo "Windscribe username must be alphanumeric (underscores allowed); ensure that the environment variable \$WINDSCRIBE_USERNAME is set properly"
+		  exit 1
+	fi
+	# password checks
+	if [[ -z "${WINDSCRIBE_PASSWORD}" ]]; then
+	  	prefixWith "[WINDSCRIBE]" echo "Unset Windscribe password; ensure that the environment variable \$WINDSCRIBE_PASSWORD is set properly"
+		  exit 1
+	fi
+	# ensure no newlines present (source: https://unix.stackexchange.com/a/276836)
+	NL='
+	'
+	case "${WINDSCRIBE_PASSWORD}"  in
+	  *"${NL}"*) prefixWith "[WINDSCRIBE]" echo "Windscribe password cannot contain new lines; ensure that the environment variable \$WINDSCRIBE_USERNAME is set properly"
+				 exit 1;;
+	esac
+	# iptable support checks
+	iptables -vnL > /dev/null 2>&1 || {
+		prefixWith "[IPTABLES]" echo "Ensure cap_add is set to NET_ADMIN"
+		  exit 1
+	}
 
 
-### Start Windscribe client
-# Windscribe performs DNS lookups remotely using locally configured DNS nameservers; use Googles
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
-# start windscribe daemon
-prefixWith "[WINDSCRIBE]" echo "Starting Windscribe client"
-prefixWith "[WINDSCRIBE]" windscribe start
-# authenticate to Windscribe
-prefixWith "[WINDSCRIBE]" echo "Authenticating to Windscribe"
-windscribe login <<- EOF
-${WINDSCRIBE_USERNAME}
-${WINDSCRIBE_PASSWORD}
-EOF
-# connect to Windscribe
-prefixWith "[WINDSCRIBE]" echo "Connecting to Windscribe"
-if [[ -n "${WINDSCRIBE_LOCATION}" ]]; then
-    prefixWith "[WINDSCRIBE]" echo "Windscribe location: ${WINDSCRIBE_LOCATION}"
-else
-    prefixWith "[WINDSCRIBE]" echo "Windscribe locations:"
-    windscribe locations
-fi
-prefixWith "[WINDSCRIBE]" windscribe connect "${WINDSCRIBE_LOCATION}"
-# prevent using untunneled internet
-prefixWith "[WINDSCRIBE]" echo "Enabling Windscribe firewall"
-prefixWith "[WINDSCRIBE]" windscribe firewall on
+	### Create SSH identity and start OpenSSH server
+	# manage SSH identity
+	prefixWith "[SSH]" echo "Creating SSH key"
+	# generate SSH key
+	prefixWith "[SSH]" ssh-keygen -N "" -f "/root/.ssh/id_rsa" <<< y
+	# allow self-connecting with SSH
+	prefixWith "[SSH]" cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
+	# start SSH server
+	prefixWith "[SSH]" echo "Starting OpenSSH server"
+	prefixWith "[SSH]" service ssh start
 
 
-### Binds SOCKS server using OpenSSH
-while true; do
-	  prefixWith "[SSH]" echo "Creating OpenSSH SOCKS server"
-	  prefixWith "[SSH]" ssh -4 -oStrictHostKeyChecking=accept-new -D 0.0.0.0:1080 -N root@127.0.0.1
-  	prefixWith "[SSH]" echo "SOCKS server died, restarting"
-  	sleep 1
-done
+	### Create TUN device for Windscribe
+	# create TUN device
+	prefixWith "[TUN]" echo "Creating TUN device"
+	prefixWith "[TUN]" mkdir -p /dev/net
+	prefixWith "[TUN]" mknod /dev/net/tun c 10 200
+	prefixWith "[TUN]" chmod 600 /dev/net/tun
+
+
+	### Start Windscribe client
+	# Windscribe performs DNS lookups remotely using locally configured DNS nameservers; use Googles
+	echo "nameserver 8.8.8.8" > /etc/resolv.conf
+	# start windscribe daemon
+	prefixWith "[WINDSCRIBE]" echo "Starting Windscribe client"
+	prefixWith "[WINDSCRIBE]" windscribe start
+	# authenticate to Windscribe
+	prefixWith "[WINDSCRIBE]" echo "Authenticating to Windscribe"
+	prefixWith "[WINDSCRIBE]"  windscribe login <<- EOF
+	${WINDSCRIBE_USERNAME}
+	${WINDSCRIBE_PASSWORD}
+	EOF
+	# connect to Windscribe
+	prefixWith "[WINDSCRIBE]" echo "Connecting to Windscribe"
+	if [[ -n "${WINDSCRIBE_LOCATION}" ]]; then
+		prefixWith "[WINDSCRIBE]" echo "Windscribe location: ${WINDSCRIBE_LOCATION}"
+	else
+		prefixWith "[WINDSCRIBE]" echo "Windscribe locations:"
+		windscribe locations
+	fi
+	prefixWith "[WINDSCRIBE]" windscribe connect "${WINDSCRIBE_LOCATION}"
+	# prevent using untunneled internet
+	prefixWith "[WINDSCRIBE]" echo "Enabling Windscribe firewall"
+	prefixWith "[WINDSCRIBE]" windscribe firewall on
+
+
+	### Binds SOCKS server using OpenSSH
+	while true; do
+		prefixWith "[SSH]" echo "Creating OpenSSH SOCKS server"
+		prefixWith "[SSH]" ssh -4 -oStrictHostKeyChecking=accept-new -D 0.0.0.0:1080 -N root@127.0.0.1
+		prefixWith "[SSH]" echo "SOCKS server died, restarting"
+		sleep 1
+	done
+
+} 2>&1 1>&3 3>&- | { while read -r line; do echo "$(date '+%Y-%m-%d %H:%M:%S') ERR $line"; done } } 3>&1 1>&2 | { while read -r line; do echo "$(date '+%Y-%m-%d %H:%M:%S') OUT $line"; done }
