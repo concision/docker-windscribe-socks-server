18536dbf613646cc06430e6936ddb3a100a98031
concision <24931310+concision@users.noreply.github.com>
2019-11-28 20:56:42 -0800

    Added README project documentation

--- deploy-container.sh
+++ deploy-container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # explicitly define variables here or source them from an .env file with --env-file flag
 #export WINDSCRIBE_DNS="1.1.1.1"
-export WINDSCRIBE_USERNAME="username"
-export WINDSCRIBE_PASSWORD="password"
+#export WINDSCRIBE_USERNAME="username"
+#export WINDSCRIBE_PASSWORD="password"
 #export WINDSCRIBE_LOCATION=""
 
 docker run \
