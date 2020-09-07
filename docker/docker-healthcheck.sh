4d7000b0fcfe763a99ad5435e5910e6bf6720943
Concision <contact@concision.me>
2020-09-07 12:10:12 -0700

    feat: removed curl test in Docker healthcheck
    Signed-off-by: Concision <contact@concision.me>

--- docker/docker-healthcheck.sh
+++ docker/docker-healthcheck.sh
@@ -2,7 +2,4 @@
 # check windscribe status
 if windscribe status | tee "$(tty)" | grep -q "DISCONNECTED"; then
     exit 1
-else
-   # check IP resolution is successful
-    curl -s "https://api.ipify.org/" || exit 1
 fi
